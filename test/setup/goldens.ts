9f56dfe50fd17b8a3360eefbc14de0b437d2e6d6
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-03-15 05:02:07 +0100

    Make golden script async

--- test/setup/goldens.ts
+++ test/setup/goldens.ts
@@ -4,24 +4,54 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-import { readFile, writeFile } from 'fs/promises';
+import { mkdir, readFile, writeFile } from 'fs/promises';
+import { dirname, join, relative } from 'path';
 import { expect } from 'vitest';
 
 /// If this is set to true, the golden files will be updated.
-export const updateGoldens = () => {
+export const shouldUpdateGoldens = () => {
   return process.env['UPDATE_GOLDENS'] === 'true';
 };
 
-export const checkGoldens = async (goldenFile: string, expected: string) => {
-  // Write golden file
-  if (updateGoldens()) {
-    await writeFile(goldenFile, expected);
-  }
+/**
+ * Updates the golden file with the given content.
+ * @param filexName - The golden file to compare with
+ * @param expected - The expected golden content
+ */
+export const expectGolden = (fileName: string) => {
+  return {
+    toBe: async (expected: any) => {
+      // Stringify json
+      if (typeof expected !== 'string') {
+        expected = JSON.stringify(expected, null, 2);
+      }
 
-  // Compare log messages with golden file
-  const golden = await readFile(goldenFile, 'utf8');
-  const message =
-    `Golden "${goldenFile}" does not match expected content. ` +
-    'Consider to rebuild goldens using "npm run updateGoldens".';
-  expect(golden, message).toBe(expected);
+      // Get golden file path
+      const goldensDir = join(__dirname, '..', 'goldens');
+      const filePath = join(goldensDir, fileName);
+      const filePathRelative = relative(process.cwd(), filePath);
+
+      // Write golden file
+      if (shouldUpdateGoldens()) {
+        await mkdir(dirname(filePath), { recursive: true });
+        await writeFile(filePath, expected);
+      }
+
+      // Check if golden file exists
+      let needsGoldenUpdate: boolean = true;
+      try {
+        const golden = await readFile(filePath, 'utf8');
+        needsGoldenUpdate = golden !== expected;
+      } catch {
+        needsGoldenUpdate = true;
+      }
+
+      // Fail if golden file needs update
+      if (needsGoldenUpdate) {
+        expect.fail(
+          `Run »pnpm updateGoldens« and review "${filePathRelative}".`,
+        );
+      }
+    },
+  };
 };
