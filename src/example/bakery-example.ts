b54c148645071172183e98a73acb342ad33c8731
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-03-21 19:20:52 +0100

    Make bakery example dynamic

--- src/example/bakery-example.ts
+++ src/example/bakery-example.ts
@@ -4,6 +4,7 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
+import { Hashed, hip } from '@rljson/hash';
 import { Json } from '@rljson/json';
 
 import { BuffetsTable } from '../content/buffet.ts';
@@ -13,6 +14,7 @@ import { IdSetsTable } from '../content/id-set.ts';
 import { PropertiesTable } from '../content/properties.ts';
 import { Rljson } from '../rljson.ts';
 import { Ref } from '../typedefs.ts';
+
 
 // .............................................................................
 export interface Ingredient extends Json {
@@ -49,114 +51,132 @@ export interface Bakery extends Rljson {
 
 // .............................................................................
 export const bakeryExample = (): Bakery => {
+  const nutritionalValues: Hashed<PropertiesTable<any>> = hip({
+    _type: 'properties',
+    _data: [
+      {
+        energy: 364,
+        fat: 0.98,
+        protein: 10.33,
+        carbohydrates: 76.31,
+        _hash: '',
+      },
+    ],
+    _hash: '',
+  });
+
+  const ingredients: Hashed<PropertiesTable<any>> = hip({
+    _type: 'properties',
+    _data: [
+      {
+        name: 'flour',
+        amountUnit: 'g',
+        nutritionalValuesRef: nutritionalValues._data[0]._hash as string,
+        _hash: '',
+      },
+    ],
+    _hash: '',
+  });
+
+  const recipeIngredients: Hashed<PropertiesTable<any>> = hip({
+    _type: 'properties',
+    _data: [
+      {
+        ingredientsRef: ingredients._data[0]._hash as string,
+        quantity: 500,
+        _hash: '',
+      },
+    ],
+    _hash: '',
+  });
+
+  const recipes: Hashed<CollectionsTable> = hip({
+    _type: 'collections',
+    _data: [
+      {
+        properties: 'recipeIngredients',
+        assign: {
+          flour: recipeIngredients._data[0]._hash as string,
+        },
+        _hash: '',
+      },
+    ],
+  });
+
+  const layers: Hashed<CollectionsTable> = hip({
+    _type: 'collections',
+    _data: [
+      {
+        properties: 'recipes',
+        assign: {
+          slice0: recipes._data[0]._hash as string,
+          slice1: recipes._data[0]._hash as string,
+        },
+      },
+    ],
+  });
+
+  const slices: Hashed<IdSetsTable> = hip({
+    _type: 'idSets',
+    _data: [
+      {
+        add: ['slice0', 'slice1'],
+        remove: [],
+        _hash: '',
+      },
+    ],
+    _hash: '',
+  });
+
+  const _idSets: Hashed<IdSetsTable> = hip({
+    _type: 'idSets',
+    _data: [
+      {
+        add: ['slice0', 'slice1'],
+        _hash: '',
+      },
+    ],
+    _hash: '',
+  });
+
+  const cakes: Hashed<CakesTable> = hip({
+    _type: 'cakes',
+    _data: [
+      {
+        idSet: _idSets._data[0]._hash as string,
+        collections: 'layers',
+        layers: {
+          flour: layers._data[0]._hash as string,
+        },
+        _hash: '',
+      },
+    ],
+  });
+
+  const buffets: BuffetsTable = hip({
+    _type: 'buffets',
+    _data: [
+      {
+        items: [
+          {
+            table: 'cakes',
+            ref: cakes._data[0]._hash as string,
+          },
+        ],
+      },
+    ],
+  });
+
   const result: Bakery = {
-    _idSets: {
-      _type: 'idSets',
-      _data: [
-        {
-          add: ['slice0', 'slice1'],
-          _hash: 'Ko990SJfgPJvNGxC63CRf7',
-        },
-      ],
-      _hash: 'NiojsJvZ7iEU7WeMWttzyJ',
-    },
-    buffets: {
-      _type: 'buffets',
-      _data: [
-        {
-          items: [
-            {
-              table: 'cakes',
-              ref: 'KdLv3zTftqKKUeqYrTtO2r',
-            },
-          ],
-        },
-      ],
-    },
-    cakes: {
-      _type: 'cakes',
-      _data: [
-        {
-          idSet: 'Ko990SJfgPJvNGxC63CRf7',
-          collections: 'layers',
-          layers: {
-            _hash: 'RBNvo1WzZ4oRRq0W9-hknp',
-          },
-          _hash: 'KdLv3zTftqKKUeqYrTtO2r',
-        },
-      ],
-    },
-    slices: {
-      _type: 'idSets',
-      _data: [
-        {
-          add: ['slice0', 'slice1'],
-          remove: [],
-          _hash: 'wyYfK5E4ArrMKQ_zvi2-EE',
-        },
-      ],
-      _hash: 'Qt6FzyzwHdEdYC3fKUXaAm',
-    },
-    layers: {
-      _type: 'collections',
-      _data: [
-        {
-          properties: 'recipes',
-          assign: {
-            slice0: 'uRTo_Jmt9lOA09e2QnwB9W',
-            slice1: 'uRTo_Jmt9lOA09e2QnwB9W',
-          },
-        },
-      ],
-    },
-    recipes: {
-      _type: 'collections',
-      _data: [
-        {
-          properties: 'recipeIngredients',
-          assign: {
-            flour: 'RCA4yzQe6mYOqquoLijKop',
-          },
-          _hash: 'uRTo_Jmt9lOA09e2QnwB9W',
-        },
-      ],
-    },
-    recipeIngredients: {
-      _type: 'properties',
-      _data: [
-        {
-          ingredientsRef: 'CdSJV-WOfnFT1svec3iJ6x',
-          quantity: 500,
-          _hash: 'RCA4yzQe6mYOqquoLijKop',
-        },
-      ],
-      _hash: 'R6dJq4ZJ3QDa9Bz8QJDhNq',
-    },
-    ingredients: {
-      _type: 'properties',
-      _data: [
-        {
-          name: 'flour',
-          amountUnit: 'g',
-          nutritionalValuesRef: 'gZXFSlrl5QAs5hOVsq5sWB',
-          _hash: 'CdSJV-WOfnFT1svec3iJ6x',
-        },
-      ],
-      _hash: 'FgJeTM0NcZvXwFcU-PD8Jf',
-    },
-    nutritionalValues: {
-      _type: 'properties',
-      _data: [
-        {
-          energy: 364,
-          fat: 0.98,
-          protein: 10.33,
-          carbohydrates: 76.31,
-          _hash: 'gZXFSlrl5QAs5hOVsq5sWB',
-        },
-      ],
-      _hash: '7k4OqQtk71w3yVGMoA9F6p',
-    },
+    _idSets,
+    buffets,
+    cakes,
+    slices,
+    layers,
+    recipes,
+    recipeIngredients,
+    ingredients,
+    nutritionalValues,
   };
 
   return result;
