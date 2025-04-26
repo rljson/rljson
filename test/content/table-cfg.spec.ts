f6a7b6a9ec4176291d5de93ed4e644c4184f946c
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-04-26 22:52:20 +0200

    Add throwOnInvalidTableCfg (#64)
    * Add throwOnInvalidTableCfg
    
    * Increase version

--- test/content/table-cfg.spec.ts
+++ test/content/table-cfg.spec.ts
@@ -10,6 +10,7 @@ import {
   exampleTableCfg,
   exampleTableCfgTable,
   TableCfg,
+  throwOnInvalidTableCfg,
 } from '../../src/content/table-cfg';
 
 import { expectGolden } from '../setup/goldens';
@@ -18,6 +19,57 @@ describe('TableCfg', () => {
   it('exampleTableCfgTable', () => {
     expectGolden('content/table-cfg-table.json').toBe(exampleTableCfgTable());
   });
+
+  describe('throwOnInvalidTableCfg', () => {
+    describe('throws an error if', () => {
+      it('the table has less than 2 columns', () => {
+        const tableCfg = exampleTableCfg({
+          columns: [{ key: '_hash', type: 'string' }],
+        });
+        expect(() => throwOnInvalidTableCfg(tableCfg)).toThrow(
+          'Invalid table configuration: ' +
+            'Table "table" must have at least a _hash and a second column',
+        );
+      });
+
+      it('the first column is not a _hash column', () => {
+        const tableCfg = exampleTableCfg({
+          columns: [
+            { key: 'a', type: 'string' },
+            { key: 'b', type: 'string' },
+          ],
+        });
+        expect(() => throwOnInvalidTableCfg(tableCfg)).toThrow(
+          'Invalid table configuration: The first column of table "table" must be "_hash"',
+        );
+      });
+
+      it('the first column is not of type string', () => {
+        const tableCfg = exampleTableCfg({
+          columns: [
+            { key: '_hash', type: 'number' },
+            { key: 'b', type: 'string' },
+          ],
+        });
+        expect(() => throwOnInvalidTableCfg(tableCfg)).toThrow(
+          'Invalid table configuration: The first _hash column of table "table" must be of type "string"',
+        );
+      });
+
+      it('a column has an unsupported type', () => {
+        const tableCfg = exampleTableCfg({
+          columns: [
+            { key: '_hash', type: 'string' },
+            { key: 'b', type: 'unknown' as any },
+          ],
+        });
+        expect(() => throwOnInvalidTableCfg(tableCfg)).toThrow(
+          'Invalid table configuration: Column "b" of table "table" has an unsupported type "unknown"',
+        );
+      });
+    });
+  });
+
   it('exampleTableCfg', () => {
     const partialCfg: Partial<TableCfg> = {
       key: 'customTable',
@@ -44,9 +96,14 @@ describe('TableCfg', () => {
 
   it('exampleTableCfg with default values', () => {
     const result = exampleTableCfg();
+    throwOnInvalidTableCfg(result);
 
     expect(result.key).toBe('table');
     expect(result.columns).toEqual([
+      {
+        key: '_hash',
+        type: 'string',
+      },
       {
         key: 'a',
         type: 'string',
