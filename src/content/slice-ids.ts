4ccaf9538f3288c71a24a9cd40290899294a85f1
heller64 <heller.maximilian@gmail.com>
2025-11-14 11:03:22 +0100

    createSliceIdsTableCfg added

--- src/content/slice-ids.ts
+++ src/content/slice-ids.ts
@@ -10,6 +10,8 @@ import { bakeryExample } from '../example/bakery-example.ts';
 import { RljsonTable } from '../rljson.ts';
 import { Ref, SliceId } from '../typedefs.ts';
 
+import { TableCfg } from './table-cfg.ts';
+
 // .............................................................................
 /**
  * An SliceIdsRef is a hash pointing to an Ids
@@ -48,3 +50,38 @@ export type SliceIdsTable = RljsonTable<SliceIds, 'sliceIds'>;
  * Returns one of the layers of the example cake
  */
 export const exampleSliceIdsTable = (): SliceIdsTable => bakeryExample().slices;
+
+// .............................................................................
+/**
+ * Creates a table configuration for slice ids table
+ * @param tableKey - the table key
+ * @returns the table configuration
+ */
+export const createSliceIdsTableCfg = (tableKey: string): TableCfg =>
+  ({
+    key: tableKey,
+    columns: [
+      { key: '_hash', type: 'string', titleLong: 'Hash', titleShort: 'Hash' },
+      {
+        key: 'base',
+        type: 'string',
+        titleLong: 'Base SliceId',
+        titleShort: 'Base SliceId',
+      },
+      {
+        key: 'add',
+        type: 'jsonArray',
+        titleLong: 'Added SliceIds',
+        titleShort: 'Add',
+      },
+      {
+        key: 'remove',
+        type: 'jsonArray',
+        titleLong: 'Removed SliceIds',
+        titleShort: 'Remove',
+      },
+    ],
+    isHead: false,
+    isRoot: false,
+    isShared: true,
+  } as TableCfg);
