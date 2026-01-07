f4babe447489c39a5e452a93d85adbab38ef2e10
heller64 <heller.maximilian@gmail.com>
2026-01-07 07:50:25 +0100

    Example codes/UT

--- test/content/tree.spec.ts
+++ test/content/tree.spec.ts
@@ -6,20 +6,20 @@
 
 import { describe, it } from 'vitest';
 
-import { createLayerTableCfg, exampleLayersTable } from '../../src/content/layer.ts';
+import { createTreesTableCfg, exampleTreesTable } from '../../src/content/tree.ts';
 
 import { expectGolden } from '../setup/goldens.ts';
 
 
 describe('TreesTable', () => {
-  it('provides a list of layers', async () => {
-    const json = exampleLayersTable();
-    await expectGolden('content/layers.json').toBe(json);
+  it('provides a simple tree w/ one root and one child', async () => {
+    const tree = exampleTreesTable();
+    await expectGolden('content/tree.json').toBe(tree);
   });
 });
-describe('createLayersTableCfg', () => {
-  it('provides a sample Layers TableCfg', async () => {
-    const tableCfg = createLayerTableCfg('example');
-    await expectGolden('content/layers-table-cfg.json').toBe(tableCfg);
+describe('createTreesTableCfg', () => {
+  it('provides a sample Tree TableCfg', async () => {
+    const tableCfg = createTreesTableCfg('example');
+    await expectGolden('content/trees-table-cfg.json').toBe(tableCfg);
   });
 });
